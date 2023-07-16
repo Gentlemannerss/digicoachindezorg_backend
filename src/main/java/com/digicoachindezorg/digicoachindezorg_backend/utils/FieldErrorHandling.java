cc0c139d6db46ee64e8da262b831c8f617c3b793
Gentlemannerss <damonvanbergen@gmail.com>
2023-07-16 19:32:22 +0200

    added utils FieldErrorHandling

--- src/main/java/com/digicoachindezorg/digicoachindezorg_backend/utils/FieldErrorHandling.java
+++ src/main/java/com/digicoachindezorg/digicoachindezorg_backend/utils/FieldErrorHandling.java
@@ -1,4 +1,17 @@
 package com.digicoachindezorg.digicoachindezorg_backend.utils;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
 public class FieldErrorHandling {
+
+    public static String getErrorToStringHandling (BindingResult bindingResult){
+        StringBuilder sb = new StringBuilder();
+        for (FieldError fe : bindingResult.getFieldErrors()){
+            sb.append("Field error: " + fe.getField() + ": ");
+            sb.append(fe.getDefaultMessage());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
