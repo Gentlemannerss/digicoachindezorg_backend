c1ce18c6f114384803aa28ccadfbd9d0ddf436a6
Gentlemannerss <damonvanbergen@gmail.com>
2023-07-16 19:08:24 +0200

    Filled all Input Dto's

--- src/main/java/com/digicoachindezorg/digicoachindezorg_backend/dtos/input/ReviewInputDto.java
+++ src/main/java/com/digicoachindezorg/digicoachindezorg_backend/dtos/input/ReviewInputDto.java
@@ -1,4 +1,25 @@
 package com.digicoachindezorg.digicoachindezorg_backend.dtos.input;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class ReviewInputDto {
+    @NotNull(message = "Score cannot be null")
+    private Integer score;
+
+    @NotBlank(message = "Review description cannot be blank")
+    private String reviewDescription;
+
+    @NotNull(message = "Customer cannot be null")
+    private Long customerId;
+
+    @NotNull(message = "Product cannot be null")
+    private Long productId;
 }
