072a23a3f59193285cb937647527bedbddd0812c
Gentlemannerss <damonvanbergen@gmail.com>
2023-07-16 19:47:45 +0200

    created CustomUserDetailsService

--- src/main/java/com/digicoachindezorg/digicoachindezorg_backend/services/CustomUserDetailsService.java
+++ src/main/java/com/digicoachindezorg/digicoachindezorg_backend/services/CustomUserDetailsService.java
@@ -1,4 +1,37 @@
 package com.digicoachindezorg.digicoachindezorg_backend.services;
 
-public class CustomUserDetailsService {
+import com.digicoachindezorg.digicoachindezorg_backend.models.Authority;
+import com.digicoachindezorg.digicoachindezorg_backend.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userService.getUserByUsername(username);
+
+        Set<Authority> authorities = user.getAuthorities();
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Authority authority: authorities) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getAuthority()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(username, user.getPassword(), grantedAuthorities);
+    }
 }
