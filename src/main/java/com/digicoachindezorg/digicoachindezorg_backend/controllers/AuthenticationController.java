8b3000b725b13b77992d65a1d9890d9f2444482b
Gentlemannerss <damonvanbergen@gmail.com>
2023-07-16 19:39:48 +0200

    created AuthenticationController

--- src/main/java/com/digicoachindezorg/digicoachindezorg_backend/controllers/AuthenticationController.java
+++ src/main/java/com/digicoachindezorg/digicoachindezorg_backend/controllers/AuthenticationController.java
@@ -1,4 +1,75 @@
 package com.digicoachindezorg.digicoachindezorg_backend.controllers;
 
+import com.digicoachindezorg.digicoachindezorg_backend.dtos.payload.AuthenticationRequest;
+import com.digicoachindezorg.digicoachindezorg_backend.dtos.payload.AuthenticationResponse;
+import com.digicoachindezorg.digicoachindezorg_backend.models.User;
+import com.digicoachindezorg.digicoachindezorg_backend.services.CustomUserDetailsService;
+import com.digicoachindezorg.digicoachindezorg_backend.services.UserService;
+import com.digicoachindezorg.digicoachindezorg_backend.utils.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.security.Principal;
+
+@CrossOrigin
+@RestController
 public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final CustomUserDetailsService userDetailsService;
+
+    private final UserService userService;
+
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, CustomUserDetailsService userDetailsService, JwtUtil jwtUtl, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtl;
+        this.userService = userService;
+    }
+
+    /*
+    Deze methode geeft de principal (basis user gegevens) terug van de ingelogde gebruiker
+    */
+    @GetMapping(value = "/authenticated")
+    public ResponseEntity<Object> authenticated(Authentication authentication, Principal principal) {
+        return ResponseEntity.ok().body(principal);
+    }
+
+    /*
+    Deze methode geeft het JWT-token terug wanneer de gebruiker de juiste inloggegevens op geeft.
+     */
+    @PostMapping(value = "/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        String username = authenticationRequest.getUsername();
+        String password = authenticationRequest.getPassword();
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+        }
+        catch (BadCredentialsException ex) {
+            throw new Exception("Incorrect username or password", ex);
+        }
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(username);
+
+        final User user = userService.getUserByUsername(username);
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt, user.getId()));
+    }
+
 }
