2686a329b05e803b49156616b6ae5f798092d219
Gentlemannerss <damonvanbergen@gmail.com>
2023-07-16 16:50:38 +0200

    opzet project

---
+++ src/test/java/com/digicoachindezorg/digicoachindezorg_backend/DigicoachindezorgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digicoachindezorg.digicoachindezorg_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigicoachindezorgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
