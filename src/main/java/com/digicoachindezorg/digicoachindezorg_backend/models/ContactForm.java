9ea07ef4ae52e04c76e58c00e0570b01fadd0fdc
Gentlemannerss <damonvanbergen@gmail.com>
2023-07-16 17:42:08 +0200

    Filled ContactForm

--- src/main/java/com/digicoachindezorg/digicoachindezorg_backend/models/ContactForm.java
+++ src/main/java/com/digicoachindezorg/digicoachindezorg_backend/models/ContactForm.java
@@ -1,4 +1,30 @@
 package com.digicoachindezorg.digicoachindezorg_backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Contact Forms")
 public class ContactForm {
+    @Id
+    @GeneratedValue
+    private Long contactFormId;
+    private String companyName;
+    private String name;
+    private Integer phoneNumber;
+    private String eMail;
+    private String description;
+    private Boolean termsOfCondition;
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "user_id")
+    private User user;
 }
