1f5909a59da91b4a083532709ab06455e17e414d
Gentlemannerss <damonvanbergen@gmail.com>
2023-07-16 19:43:15 +0200

    created ReviewController

--- src/main/java/com/digicoachindezorg/digicoachindezorg_backend/controllers/ReviewController.java
+++ src/main/java/com/digicoachindezorg/digicoachindezorg_backend/controllers/ReviewController.java
@@ -1,4 +1,47 @@
 package com.digicoachindezorg.digicoachindezorg_backend.controllers;
 
+import com.digicoachindezorg.digicoachindezorg_backend.dtos.input.ReviewInputDto;
+import com.digicoachindezorg.digicoachindezorg_backend.dtos.output.ReviewOutputDto;
+import com.digicoachindezorg.digicoachindezorg_backend.exceptions.RecordNotFoundException;
+import com.digicoachindezorg.digicoachindezorg_backend.services.ReviewService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+    @PostMapping
+    public ResponseEntity<ReviewOutputDto> createReview(@RequestBody ReviewInputDto reviewInputDto) throws RecordNotFoundException { /*@PathVaribale OF @RequestBody voor de userID*/
+        ReviewOutputDto reviewDto = reviewService.createReview(reviewInputDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewDto);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ReviewOutputDto> updateReview(@PathVariable Long id, @RequestBody ReviewInputDto reviewInputDto) throws RecordNotFoundException {
+        ReviewOutputDto updatedReview = reviewService.updateReview(id, reviewInputDto);
+        return ResponseEntity.ok(updatedReview);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReview(@PathVariable Long id) throws RecordNotFoundException {
+        reviewService.deleteReview(id);
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ReviewOutputDto> getReview(@PathVariable Long id) throws RecordNotFoundException {
+        ReviewOutputDto reviewDto = reviewService.getReview(id);
+        return ResponseEntity.ok(reviewDto);
+    }
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<List<ReviewOutputDto>> getAllReviewsPerProduct(@PathVariable Long productId) {
+        List<ReviewOutputDto> reviews = reviewService.getAllReviewsPerProduct(productId);
+        return ResponseEntity.ok(reviews);
+    }
 }
